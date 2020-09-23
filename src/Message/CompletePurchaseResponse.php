f5869d18897046ea5596fe9e7b65abf4db662620
Poonam Thakur <69500540+Poonam0311@users.noreply.github.com>
2020-09-23 16:50:41 +0530

    Update and rename Response.php to CompletePurchaseResponse.php

--- src/Message/CompletePurchaseResponse.php
+++ src/Message/CompletePurchaseResponse.php
@@ -5,17 +5,17 @@ namespace Omnipay\Mobikwikpg\Message;
 use Omnipay\Common\Message\AbstractResponse;
 
 /**
- * Response
+ * CompletePurchaseResponse
  */
-class Response extends AbstractResponse
+class CompletePurchaseResponse extends AbstractResponse
 {
     /**
      * @return bool
      */
     public function isSuccessful()
     {
-        if (isset($this->data->success)) {
-            return $this->data->success;
+        if (isset($this->data['success'])) {
+            return $this->data['success'];
         }
 
         return false;
